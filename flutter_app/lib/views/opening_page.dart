f2d283172478cddf17959035a02bdf430852d40c
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2024-07-12 11:46:16 +0700

    add check token on opening page, modify as splash screen

--- flutter_app/lib/views/opening_page.dart
+++ flutter_app/lib/views/opening_page.dart
@@ -1,19 +1,50 @@
-// lib/opening_page.dart
-
 import 'package:flutter/material.dart';
+import 'dart:async'; // for Timer
+import 'package:shared_preferences/shared_preferences.dart'; // for SharedPreferences
+import '../../core/app_export.dart';
 
-class OpeningPage extends StatelessWidget {
+class OpeningPage extends StatefulWidget {
+  @override
+  _OpeningPageState createState() => _OpeningPageState();
+}
+
+class _OpeningPageState extends State<OpeningPage> {
+  @override
+  void initState() {
+    super.initState();
+    _checkTokenAndNavigate();
+  }
+
+  Future<void> _checkTokenAndNavigate() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    String? token = pref.getString('token');
+
+    // Wait for 2 seconds before navigating
+    await Future.delayed(Duration(seconds: 2));
+
+    if (token != null) {
+      // Navigate to the main screen (replace '/main' with your main screen route)
+      Navigator.of(context).pushReplacementNamed(AppRoutes.loginPage);
+    } else {
+      // Navigate to the welcoming screen
+      Navigator.of(context).pushReplacementNamed(AppRoutes.welcomingPage);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color(0XFFA7FF87),
-            Color(0XFFFFFFFF)
-          ])),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0XFFA7FF87),
+              Color(0XFFFFFFFF)
+            ],
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -27,7 +58,7 @@ class OpeningPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             Image.asset(
-              'assets/images/opening_logo.png', // Ensure you have this image in your assets folder
+              ImageConstant.openingLogoPath, // Ensure you have this image in your assets folder
               width: 200,
               height: 200,
             ),
