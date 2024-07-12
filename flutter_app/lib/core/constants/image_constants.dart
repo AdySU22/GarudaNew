18ebf79fa8fe3008ec229fbe7c178e012a969682
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2024-07-12 11:46:47 +0700

    define paths for images

--- flutter_app/lib/core/constants/image_constants.dart
+++ flutter_app/lib/core/constants/image_constants.dart
@@ -1,4 +1,6 @@
+//Path for Images
 class ImageConstant {
   static String imagePath = "assets/images";
   static String openingLogoPath = "$imagePath/opening_logo.png";
+  static String welcomingImgPath = "$imagePath/welcoming_img.png";
 }
